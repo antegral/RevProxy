a54b6f6117a8a146be657a2fa5da9d8dbbf3a466
antegral <antegral@antegral.net>
2022-10-24 21:58:18 +0900

    Remove superfluous w.WriteHeader() logic

--- src/Proxy/Proxy.go
+++ src/Proxy/Proxy.go
@@ -9,23 +9,27 @@ import (
 )
 
 func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
+	Log.Verbose.Print("NewProxy > targetHost: ", targetHost)
 	if url, err := url.Parse(targetHost); err != nil {
 		return nil, err
 	} else {
+		Log.Verbose.Print("NewProxy > url.path: ", url.Path)
 		return httputil.NewSingleHostReverseProxy(url), nil
 	}
 }
 
-func RequestHandler(proxy *httputil.ReverseProxy, Password string) func(http.ResponseWriter, *http.Request) {
+func RequestHandler(proxy *httputil.ReverseProxy, Password string, ProxyUrl *url.URL) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		PassHeader := r.Header.Get("X-RevProxy-Token")
+
+		r.Header.Add("Host", ProxyUrl.Hostname())
+
 		if len(Password) <= 0 {
+			Log.Info.Print("[", r.Method, "/OK] ", r.RemoteAddr, " > ", r.RequestURI)
 			proxy.ServeHTTP(w, r)
-		}
-		if PassHeader != Password {
-			w.WriteHeader(403)
-			w.Write([]byte("Forbidden Page"))
+		} else if PassHeader != Password {
 			Log.Warn.Print("[", r.Method, "/FAIL] ", r.RemoteAddr, " > ", r.RequestURI)
+			w.Write([]byte("Forbidden Page"))
 		} else {
 			Log.Info.Print("[", r.Method, "/OK] ", r.RemoteAddr, " > ", r.RequestURI)
 			proxy.ServeHTTP(w, r)
